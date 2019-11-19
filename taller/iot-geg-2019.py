e2f90a6d3112b17f3eaee2de5851244a2ddee5ad
gnconsult <gnpitty@gmail.com>
2019-11-19 15:04:44 -0500

    mejoras en iot-geg-2019.py

--- taller/iot-geg-2019.py
+++ taller/iot-geg-2019.py
@@ -1,18 +1,18 @@
-from pathlib import Path
 from time import sleep
 import uuid
 from time import sleep
 from google.cloud import storage
 
+PREVIEW_DELAY=2
 BUCKET100 = 'ejemplo_gncon_001'
 IMG_PATH = 'imagenes'
-DEBUG= True
+DEBUG= False
 
 def tomaFoto( imagenFile):
     from picamera import PiCamera
     camera = PiCamera()
     camera.start_preview()
-    sleep(5)
+    sleep(PREVIEW_DELAY)
     camera.capture(IMG_PATH+'/'+imagenFile)
     camera.stop_preview()
 
@@ -26,9 +26,10 @@ def upload(imageFile):
 
 s3file =  str(uuid.uuid4().hex)+".jpg"
 print ('Procesando: '+s3file)
-#tomaFoto( s3file)
-if DEBUG:
-    s3file = 'image.jpg'
+tomaFoto( s3file)
+
+#if DEBUG:
+#    s3file = 'image.jpg'
 
 upload(s3file)
 
--- taller/start.sh
+++ taller/start.sh
@@ -1,4 +1,3 @@
 cd ~/iot/geg-iot-2019/taller
 source env3/bin/activate
-python camera3.py
-gsutil cp image.jpg gs://ejemplo_gncon_001/image2.jpg
+python iot-geg-2019.py
